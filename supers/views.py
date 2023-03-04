f3f42ad142ecb67615abf51b34a8d009a0f074c9
JValenueva <rykerluno@gmail.com>
2023-03-03 19:42:16 -0700

    generated POST, PUT, DELETE request methods in supers views file + migrated modifications to super_types models file

--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -11,6 +12,7 @@ def supers_list(request):
         
         super_type_name= request.query_params.get('super_type')
         print(super_type_name)
+        
         queryset = Super.objects.all()
 
 
@@ -26,15 +28,18 @@ def supers_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     
-@api_view(['GET'])
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def supers_detail(request, pk):
-    try:
-        
-        super = Super.objects.get(pk=pk)
+    super = get_object_or_404(Super, pk=pk)
+    if request.method == 'GET':
         serializer = SuperSerializer(super);
         return Response(serializer.data)
-    
-    except Super.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    
+    elif request.method == 'PUT':
+        serializer = SuperSerializer(super, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        super.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
