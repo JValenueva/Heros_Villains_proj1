8ed8e16dee5a153883f8b9b0f1f12486090bac8c
JValenueva <rykerluno@gmail.com>
2023-03-07 18:16:27 -0700

    finished modifying views file in supers app + modified get_all_supers request method in post man

--- supers/views.py
+++ supers/views.py
@@ -9,17 +9,14 @@ from .models import Super
 @api_view(['GET', 'POST'])
 def supers_list(request):
     if request.method == 'GET':
-        
-        super_type_name= request.query_params.get('super_type')
-        print(super_type_name)
-        
-        queryset = Super.objects.all()
+        super_type_param = request.query_params.get('super_type')
+        print(super_type_param)
+        supers = Super.objects.all()
 
+        if super_type_param:
+            supers = supers.filter(super_type__type=super_type_param)
 
-        if super_type_name:
-            queryset = queryset.filter(super_type__name=super_type_name)      
-
-        serializer = SuperSerializer(queryset, many = True)
+        serializer = SuperSerializer(supers, many=True)
         return Response(serializer.data)
     
     elif request.method == 'POST':
